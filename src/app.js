faf710187eec5c273a8f38bf8eccdacfa20eb72f
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-11-19 18:44:31 +0530

    Apollo Client Hookup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
 function App() {
-  return <div>Hi World!</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h1>Hi World!</h1>
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
